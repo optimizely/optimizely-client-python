5515fc7207404013ea9ba655b29bc414320971d9
Andy Harris <andy.harris@optimizely.com>
2014-09-23 19:15:08 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='optimizely',
+      version='0.1',
+      description='An interface to Optimizely\'s REST API.',
+      url='https://github.com/optimizely/optimizely-client-python',
+      author='Optimizely',
+      packages=['optimizely'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
