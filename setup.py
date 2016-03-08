b6fe5c545664467f2592fa14714e1efe2ac13ad2
Matt Auerbach <matthew.auerbach@optimizely.com>
2016-03-07 19:06:32 -0800

    syntax & name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='optimizely-python',
+setup(name='optimizely',
       version='0.3',
       description='A Python interface to Optimizely\'s REST API.',
       url='https://github.com/optimizely/optimizely-client-python',
       author='Optimizely',
       author_email = 'developers@optimizely.com',
-      packages=['optimizely-python'],
-      keywords= ['python-sdk', 'optimizely']
-      download_url = 'https://github.com/optimizely/optimizely-client-python/tarball/0.3'
+      packages=['optimizely'],
+      keywords= ['python-sdk', 'optimizely'],
+      download_url = 'https://github.com/optimizely/optimizely-client-python/tarball/0.3',
       install_requires=[
         'requests',
       ],
