4a7d2ea9e6a01ce13ec3577438b2085a61f11630
Matt Auerbach <tennisfan0526@gmail.com>
2016-06-06 23:29:55 -0700

    wip

--- optimizely/client.py
+++ optimizely/client.py
@@ -12,10 +12,11 @@ BASE_URL = 'https://www.optimizelyapis.com/experiment/v1/'
 class Client(object):
   ALLOWED_REQUESTS = ['get', 'post', 'put', 'delete']
 
-  def __init__(self, api_key, api_base=BASE_URL):
+  def __init__(self, api_key, api_base=BASE_URL, token_type):
     # set API information
     self.api_key = api_key
     self.api_base = api_base
+    self.token_type = token_type 
 
     # instantiate resource generators for the relevant API resources
     self.Projects = resource.ResourceGenerator(client=self, resource=resource.Project)
@@ -34,10 +35,12 @@ class Client(object):
       headers = headers or {}
 
       # test if Oauth token
-      if ":" in self.api_key:
+      if self.token_type == 'legacy':
         headers.update({'Token': self.api_key, 'User-Agent': 'optimizely-client-python/0.1.1'})
-      else:
+      elif self.token_type == 'oauth':
         headers.update({'Authorization': ' Bearer ' + self.api_key, 'User-Agent': 'optimizely-client-python/0.1.1'})
+      else:
+        raise error.BadRequestError('%s is not a valid token type.' % token_type)
       # make request and return parsed response
       url = urlparse.urljoin(self.api_base, '/'.join([str(url_part) for url_part in url_parts]))
       return self.parse_response(getattr(requests, method)(url, headers=headers, data=data))
