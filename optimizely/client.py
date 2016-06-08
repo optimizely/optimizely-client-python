17c68b346446959984f8e244498472cc25e28e9a
Matt Auerbach <tennisfan0526@gmail.com>
2016-06-07 22:35:04 -0700

    fixed token type validation and error handling

--- optimizely/client.py
+++ optimizely/client.py
@@ -8,15 +8,20 @@ from optimizely import resource
 
 BASE_URL = 'https://www.optimizelyapis.com/experiment/v1/'
 
+VALID_TOKEN_TYPES = ('legacy', 'oauth')
 
 class Client(object):
   ALLOWED_REQUESTS = ['get', 'post', 'put', 'delete']
 
-  def __init__(self, api_key, api_base=BASE_URL, token_type):
+  def __init__(self, api_key, token_type, api_base=BASE_URL):
     # set API information
     self.api_key = api_key
     self.api_base = api_base
-    self.token_type = token_type 
+
+    if token_type in VALID_TOKEN_TYPES:
+      self.token_type = token_type
+    else:
+      raise ValueError('Invalid Token Type')
 
     # instantiate resource generators for the relevant API resources
     self.Projects = resource.ResourceGenerator(client=self, resource=resource.Project)
@@ -40,7 +45,7 @@ class Client(object):
       elif self.token_type == 'oauth':
         headers.update({'Authorization': ' Bearer ' + self.api_key, 'User-Agent': 'optimizely-client-python/0.1.1'})
       else:
-        raise error.BadRequestError('%s is not a valid token type.' % token_type)
+        raise ValueError('%s is not a valid token type.' % token_type)
       # make request and return parsed response
       url = urlparse.urljoin(self.api_base, '/'.join([str(url_part) for url_part in url_parts]))
       return self.parse_response(getattr(requests, method)(url, headers=headers, data=data))
